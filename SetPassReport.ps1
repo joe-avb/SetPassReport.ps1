97754fb0db9f141961645ee270290e7d7c9c5a9f
joe-avb <93931732+joe-avb@users.noreply.github.com>
2021-11-08 10:40:00 -0500

    Add files via upload

---
+++ SetPassReport.ps1
@@ -0,0 +1,5 @@
+﻿import-module activedirectory
+get-aduser -filter * -properties passwordlastset, EmailAddress, enabled | Where { $_.Enabled -eq $True } | 
+select name, EmailAddress, passwordlastset  | 
+Sort-Object -Property passwordlastset |
+export-csv "Path\To\Pass_last_set_$(get-date -f yyyy-MM-dd-hhmm).csv"
